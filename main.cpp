2501284aa47cc28e62f0255a62f551cc41f75754
Guilherme Krieger Ferreira <guilhermekf7@gmail.com>
2025-11-26 11:44:44 -0300

    Colocando ranking e novas texturas

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "raylib.h"
 #include <iostream>
 #include <vector>
+#include <ctime>
+#include <fstream>
 
 #define LEFT 2
 #define RIGHT 3
@@ -19,9 +21,9 @@ const int layoutF1[5][10] =
 {
     {1,1,1,1,1,1,1,1,1,1},
     {1,1,1,1,1,1,1,1,1,1},
-    {1,1,1,1,1,1,1,1,1,1},
-    {1,1,1,1,1,1,1,1,1,1},
-    {2,2,2,2,2,2,2,2,2,2}
+    {2,2,2,2,2,2,2,2,2,2},
+    {2,2,2,2,2,2,2,2,2,2},
+    {3,3,3,3,3,3,3,3,3,3}
 };
 
 const int layoutF2[5][10] =
@@ -42,6 +44,8 @@ const int layoutF3[5][10] =
     {2,1,3,2,1,3,2,1,3,2}
 };
 
+const char separador = ';';
+
 struct Hitbox{
     float x, y;
     float largura, altura;
@@ -52,9 +56,11 @@ struct Bloco{
     float x, y;
     float largura, altura;
     int vidas;
-    Color cor;
+    //Color cor;
     bool ativo;
     Hitbox direita, esquerda, cima, baixo;
+    Texture2D nomeTextura;
+    int pontos;
 };
 
 struct Bola{
@@ -62,6 +68,15 @@ struct Bola{
     Vector2 vel;
     float raio;
     bool ativo;
+};
+
+struct Ranking{
+    string nome; 
+    string data;
+    string tempo;
+    int pontuacao;
+    string hora_inicio;
+    string hora_fim;
 };
 
 void colocarBolaNoPaddle(vector<Bola> &bolas, const Vector2 &posPaddle, const Vector2 tamPaddle){
@@ -118,7 +133,7 @@ void movimentarBola(Bola &b, const float dt){
     }
 }
 
-void checarColisoes(Bola &b, const Rectangle &paddle, vector<Bloco> &blocos){
+void checarColisoes(Bola &b, const Rectangle &paddle, vector<Bloco> &blocos, Ranking &jogoAtual){
     if(!b.ativo) return;
 
     if(CheckCollisionCircleRec(b.pos, b.raio, paddle)){ 
@@ -149,9 +164,10 @@ void checarColisoes(Bola &b, const Rectangle &paddle, vector<Bloco> &blocos){
         if(colisao){
             bl.vidas--;
 
-            if(bl.vidas == 2) bl.cor = GREEN;
-            else if(bl.vidas == 1) bl.cor = YELLOW;
-            if(bl.vidas == 0) bl.ativo = false;
+            if(bl.vidas == 0){
+                bl.ativo = false;
+                jogoAtual.pontuacao += bl.pontos;
+            }
 
             return;
         }
@@ -159,7 +175,7 @@ void checarColisoes(Bola &b, const Rectangle &paddle, vector<Bloco> &blocos){
     }
 }
 
-void carregarFases(vector<Bloco> &blocos, int fase){
+void carregarFases(vector<Bloco> &blocos, int fase, Texture2D textura4, Texture2D textura5, Texture2D textura6){
     blocos.clear();
 
     int linhas = 5;
@@ -208,16 +224,27 @@ void carregarFases(vector<Bloco> &blocos, int fase){
             b.baixo.largura = largura-2;
             b.baixo.altura = hitboxSize;
 
-            if(b.vidas == 1) b.cor = YELLOW;
-            else if(b.vidas == 2) b.cor = GREEN;
-            else b.cor = RED;
+            //b.cor = BLACK;
+
+            if(b.vidas == 1){
+                b.pontos = 50;
+                b.nomeTextura = textura4;
+            } 
+            else if(b.vidas == 2){
+                b.pontos = 100;
+                b.nomeTextura = textura5;
+            }
+            else{
+                b.pontos = 200;
+                b.nomeTextura = textura6;
+            }
 
             blocos.push_back(b);
         }
     }
 }
 
-int Menu(){
+int Menu(Texture2D texture2){
     Rectangle botaoJogar = {250, 250, 300, 80};
     Rectangle botaoDificuldade = {250, 350, 300, 80};
     Rectangle botaoRanking = {250, 450, 300, 80};
@@ -237,6 +264,8 @@ int Menu(){
         if (CheckCollisionPointRec(mouse, botaoSair)) return 3;                 
     }
 
+    DrawTexture(texture2, SCREENWIDTH/2 - texture2.width/2, SCREENHEIGHT/2 - texture2.height/2, WHITE);
+
     DrawRectangleRec(botaoJogar, corJogar);
     DrawRectangleRec(botaoDificuldade, corDificuldade);
     DrawRectangleRec(botaoRanking, corRanking);
@@ -250,7 +279,7 @@ int Menu(){
     return 0;
 }
 
-int MenuDificuldade(){
+int MenuDificuldade(Texture2D texture2){
     Rectangle botaoFacil = {250, 300, 300, 80};
     Rectangle botaoMedio = {250, 400, 300, 80};
     Rectangle botaoDificil = {250, 500, 300, 80};
@@ -267,6 +296,9 @@ int MenuDificuldade(){
         if(CheckCollisionPointRec(mouse, botaoDificil)) return 3;
     }
 
+    DrawTexture(texture2, SCREENWIDTH/2 - texture2.width/2, SCREENHEIGHT/2 - texture2.height/2, WHITE);
+
+
     DrawRectangleRec(botaoFacil, corFacil);
     DrawRectangleRec(botaoMedio, corMedio);
     DrawRectangleRec(botaoDificil, corDificil);
@@ -278,6 +310,172 @@ int MenuDificuldade(){
     return 0;
 }
 
+void inicializarVetorRanking(vector<Ranking> &ranking){
+    ranking.clear();
+
+    ifstream arqRank("../Arkanoid/Ranking.txt");
+    if(arqRank.is_open()){
+        string linha;
+
+        getline(arqRank, linha);
+
+        while(getline(arqRank, linha)){
+            if(linha.empty()) continue;
+
+            int p1 = linha.find(';');
+            int p2 = linha.find(';', p1 + 1);
+            int p3 = linha.find(';', p2 + 1);
+
+            if (p1 == -1 || p2 == -1 || p3 == -1) continue;
+
+            string snome = linha.substr(0,p1);
+            string sData = linha.substr(p1 + 1, p2 - p1 - 1);
+            string sTempo = linha.substr(p2 + 1, p3 - p2 - 1);
+            string sPontuacao = linha.substr(p3 + 1);
+
+            Ranking rank;
+            rank.nome = snome;
+            rank.data = sData;
+            rank.tempo = sTempo;
+            rank.pontuacao = stoi(sPontuacao);
+
+            ranking.push_back(rank);
+        }
+        arqRank.close();
+    }
+}
+
+void ordenarVetorRanking(vector<Ranking> &ranking){
+    for(int i = 0; i < (int)ranking.size(); i++){
+        for(int j = i + 1; j < (int)ranking.size(); j++){
+            if(ranking[j].pontuacao > ranking[i].pontuacao){
+                swap(ranking[i], ranking[j]);
+            }
+        }
+    }
+}
+
+void atualizarArquivos(vector<Ranking> &ranking){
+    ofstream rank;
+    rank.open("../Arkanoid/Ranking.txt");
+
+    if(!rank.is_open()){
+        cout<<"Erro ao tentar abrir o arquivo."<<endl;
+        return;
+    }else{
+        rank<<"Nome;Data;Tempo;Pontuação"<<endl;
+
+        for(int i = 0; i < (int)ranking.size(); i++){
+            rank<<ranking[i].nome << separador
+                <<ranking[i].data << separador
+                <<ranking[i].tempo <<separador
+                <<ranking[i].pontuacao <<endl;
+        }
+        rank.close();
+    }
+}
+
+void mostrarRanking(vector<Ranking> &ranking, Texture2D texture3){
+    BeginDrawing();
+    ClearBackground(BLACK);
+    DrawTexture(texture3, SCREENWIDTH/2 - texture3.width/2, SCREENHEIGHT/2 - texture3.height/2, WHITE);
+    DrawText("RANKING", 285, 40, 45, YELLOW);
+    
+    DrawText("POS", 80, 130, 22, WHITE);
+    DrawText("NOME", 160, 130, 22, WHITE);
+    DrawText("DATA", 340, 130, 22, WHITE);
+    DrawText("TEMPO", 480, 130, 22, WHITE);
+    DrawText("PONTOS", 600, 130, 22, WHITE);
+
+    DrawLine(60, 160, 740, 160, WHITE);
+
+    int y = 180;
+
+    for(int i=0; i < (int)ranking.size() && i < 10; i++){
+        char posStr[5];
+        sprintf(posStr, "%dº", i+1);
+
+        DrawText(posStr, 80, y, 20, YELLOW);
+        DrawText(ranking[i].nome.c_str(),  160,  y, 20, WHITE);
+        DrawText(ranking[i].data.c_str(),  340,  y, 20, WHITE);
+        DrawText(ranking[i].tempo.c_str(), 480, y, 20, WHITE);
+
+        char pts[10];
+        sprintf(pts, "%d", ranking[i].pontuacao);
+        DrawText(pts, 600, y, 20, WHITE);
+
+        y += 32;
+    }
+
+    DrawText("Pressione ENTER para voltar",230, 720, 25, YELLOW);
+    EndDrawing();
+}
+
+void salvarRanking(bool &rankingSalvo, Ranking &jogoAtual, vector<Ranking> &ranking, time_t jogoInicio, bool &inserindoNome, string &nomeDigitado){
+    if(rankingSalvo) return;
+
+    if(!inserindoNome){
+        inserindoNome = true;
+        nomeDigitado = "";
+    }
+
+    DrawText("Digite seu nome (max 12 caracteres):", 200, 470, 26, YELLOW);
+    DrawRectangle(250, 510, 300, 38, WHITE);
+    DrawRectangleLines(250, 510, 300, 38, BLACK);
+
+    int key = GetCharPressed();
+        
+    while(key > 0){
+        if((key >= 32 && key <= 125) && nomeDigitado.size() < 12){
+            nomeDigitado.push_back((char)key);
+        }
+        key = GetCharPressed();
+    }
+
+    if(IsKeyPressed(KEY_BACKSPACE) && !nomeDigitado.empty()){
+        nomeDigitado.pop_back();
+    }
+
+    DrawText(nomeDigitado.c_str(), 260, 515, 28, BLACK);
+    DrawText("Pressione ENTER para confirmar", 250, 560, 20, YELLOW);
+
+    if(IsKeyPressed(KEY_ENTER) && nomeDigitado.size() > 0){
+        jogoAtual.nome = nomeDigitado;
+
+        ranking.push_back(jogoAtual);
+        ordenarVetorRanking(ranking);
+        atualizarArquivos(ranking);
+
+        rankingSalvo = true;
+        inserindoNome = false;
+    }
+}
+
+void salvarTempo(Ranking &jogoAtual, bool &tempoRegistrado, time_t jogoInicio){
+    if(tempoRegistrado) return;
+
+    time_t agora = time(0);
+    tm *tempo = localtime(&agora);
+
+    char dataStr[11];
+    char horaFim[9];
+    strftime(dataStr, sizeof(dataStr), "%d/%m/%Y", tempo);
+    strftime(horaFim, sizeof(horaFim), "%H:%M:%S", tempo);
+
+    jogoAtual.data = dataStr;
+    jogoAtual.hora_fim = horaFim;
+
+    double duracaoSegs = 0;
+    if(jogoInicio != 0) duracaoSegs = difftime(agora, jogoInicio);
+
+    char tempoStr[10];
+    sprintf(tempoStr, "%02d:%02d", (int)(duracaoSegs / 60), (int)(duracaoSegs) % 60);
+
+    jogoAtual.tempo = tempoStr;
+
+    tempoRegistrado = true;
+}
+
 int main() {
     InitWindow(SCREENWIDTH, SCREENHEIGHT, "Arkanoid");
     SetTargetFPS(60);     
@@ -287,9 +485,37 @@ int main() {
 
     vector<Bola> bolas;
     
-    Image image = LoadImage("Imagens/background.png");
-    Texture2D texture = LoadTextureFromImage(image);
-    UnloadImage(image);
+    Image backgound_jogo = LoadImage("Imagens/background.png");
+    Texture2D texture = LoadTextureFromImage(backgound_jogo);
+    UnloadImage(backgound_jogo);
+
+    Image background_menu = LoadImage("Imagens/background_menu.png");
+    Texture2D texture2 = LoadTextureFromImage(background_menu);
+    UnloadImage(background_menu);
+
+    Image background_ranking = LoadImage("Imagens/background_ranking.png");
+    Texture2D texture3 = LoadTextureFromImage(background_ranking);
+    UnloadImage(background_ranking);
+
+    Image bloco1 = LoadImage("Imagens/bloco1.png");
+    Texture2D texture4 = LoadTextureFromImage(bloco1);
+    UnloadImage(bloco1);
+
+    Image bloco2 = LoadImage("Imagens/bloco2.png");
+    Texture2D texture5 = LoadTextureFromImage(bloco2);
+    UnloadImage(bloco2);
+
+    Image bloco3 = LoadImage("Imagens/bloco3.png");
+    Texture2D texture6 = LoadTextureFromImage(bloco3);
+    UnloadImage(bloco3);
+
+    Image bola = LoadImage("Imagens/bola.png");
+    Texture2D texture7 = LoadTextureFromImage(bola);
+    UnloadImage(bola);
+
+    Image paddle = LoadImage("Imagens/paddle.png");
+    Texture2D texture8 = LoadTextureFromImage(paddle);
+    UnloadImage(paddle);
 
     int estadoTela = 0;
 
@@ -299,7 +525,19 @@ int main() {
     bool resetFase = true;
     int vidas = 3;
     int lastPos = RIGHT;
-  
+
+    vector<Ranking> ranking;
+    Ranking jogoAtual;
+
+    inicializarVetorRanking(ranking);
+
+    jogoAtual.nome = "PLAYER";
+    time_t jogoInicio = 0;
+    bool rankingSalvo = false;
+    string nomeDigitado = "";
+    bool inserindoNome = false;
+    bool tempoRegistrado = false;
+    
     while (!WindowShouldClose()) {
         float dt = GetFrameTime();
     
@@ -315,18 +553,38 @@ int main() {
         if (estadoTela == 0){
             BeginDrawing();
             ClearBackground(BLACK);
-            estadoTela = Menu();     
+            estadoTela = Menu(texture2);     
             EndDrawing();
             continue;
         }
 
         if(estadoTela == 1){
-
             if(resetFase){
-                carregarFases(blocos, faseAtual);
+                carregarFases(blocos, faseAtual, texture4, texture5, texture6);
 
                 bolas.clear();
                 colocarBolaNoPaddle(bolas, posPaddle, tamPaddle);
+
+                jogoAtual.pontuacao = 0;
+
+                time_t agora = time(0);
+                jogoInicio = agora;
+
+                tm *tempo = localtime(&agora);
+                char horaInicio[9];
+                strftime(horaInicio, sizeof(horaInicio), "%H:%M:%S", tempo);
+
+                jogoAtual.hora_inicio = horaInicio;
+
+                rankingSalvo = false;
+                tempoRegistrado = false;
+                inserindoNome = false;
+                nomeDigitado.clear();
+
+                jogoAtual.data = "";
+                jogoAtual.hora_fim = "";
+                jogoAtual.tempo = "00:00";
+                jogoAtual.nome = "PLAYER";
 
                 resetFase = false;
             }
@@ -336,20 +594,23 @@ int main() {
             DrawTexture(texture, SCREENWIDTH/2 - texture.width/2, SCREENHEIGHT/2 - texture.height/2, WHITE);
 
             movimentarPaddle(posPaddle, lastPos, dt);
-            lastPos = 0;
+
+            DrawTexture(texture8, posPaddle.x, posPaddle.y, WHITE);
 
             Rectangle paddle = {posPaddle.x, posPaddle.y, tamPaddle.x, tamPaddle.y };
 
             for(int i = 0; i < (int)bolas.size(); i++){
                 movimentarBola(bolas[i], dt);
-                checarColisoes(bolas[i], paddle, blocos);
+                checarColisoes(bolas[i], paddle, blocos, jogoAtual);
             }
-
-            DrawRectangleV(posPaddle, tamPaddle, RED);
 
             for(int i = 0; i < (int)bolas.size(); i++){
                 if(bolas[i].ativo){
-                    DrawCircleV(bolas[i].pos, bolas[i].raio, RED);
+                    Rectangle areaTexturaBola = {0, 0, (float)texture7.width, (float)texture7.height};
+                    Rectangle areaTelaBola = {bolas[i].pos.x - bolas[i].raio, bolas[i].pos.y - bolas[i].raio, bolas[i].raio*2, bolas[i].raio*2};
+                    Vector2 pontoDeRotacaoBola = {0, 0};
+                    DrawTexturePro(texture7, areaTexturaBola, areaTelaBola, pontoDeRotacaoBola, 0.0f, WHITE);
+
                 }
             }
 
@@ -363,11 +624,17 @@ int main() {
                     Rectangle hitboxCima = {b.cima.x, b.cima.y, b.cima.largura, b.cima.altura};
                     Rectangle hitboxBaixo = {b.baixo.x, b.baixo.y, b.baixo.largura, b.baixo.altura};
                     
-                    DrawRectangleRec(hitboxEsq, b.cor);
-                    DrawRectangleRec(hitboxDir, b.cor);
-                    DrawRectangleRec(hitboxCima, b.cor);
-                    DrawRectangleRec(hitboxBaixo, b.cor);
-                    DrawRectangleRec(r, b.cor);
+                    //DrawRectangleRec(hitboxEsq, b.cor);
+                    //DrawRectangleRec(hitboxDir, b.cor);
+                    //DrawRectangleRec(hitboxCima, b.cor);
+                    //DrawRectangleRec(hitboxBaixo, b.cor);
+
+                    Rectangle areaDaTextura = {0, 0, (float)b.nomeTextura.width, (float)b.nomeTextura.height};
+                    Rectangle areaNaTela = {b.x, b.y, b.largura, b.altura};           
+                    Vector2 pontoDeRotacao = {0, 0};
+
+                    DrawTexturePro(b.nomeTextura, areaDaTextura, areaNaTela, pontoDeRotacao, 0.0f, WHITE);
+
 
                 }
             }
@@ -381,6 +648,7 @@ int main() {
                 vidas--;
                 if(vidas <= 0){
                     estadoTela = 5; //Derrota
+                    salvarTempo(jogoAtual, tempoRegistrado, jogoInicio);
                 }else{
                     bolas.clear();
                     colocarBolaNoPaddle(bolas, posPaddle, tamPaddle);
@@ -398,23 +666,20 @@ int main() {
 
             if(terminouFase){
                 faseAtual++;
-                if(faseAtual > 3) estadoTela = 4; // Vitoria
-                else resetFase = true;
+                if(faseAtual > 3){
+                    estadoTela = 4; // Vitoria
+                    salvarTempo(jogoAtual, tempoRegistrado, jogoInicio);
+                }else resetFase = true;
             }
             DrawFPS(40, 30);
             EndDrawing();
         }
 
         if(estadoTela == 2){
-            BeginDrawing();
-            ClearBackground(BLACK);
-
-            DrawText("RANKING", 300, 200, 50, YELLOW);
-            DrawText("Pressione ENTER para voltar ao menu", 150, 400, 30, WHITE);
+            mostrarRanking(ranking, texture3);
 
             if(IsKeyPressed(KEY_ENTER)) estadoTela = 0;
 
-            EndDrawing();
             continue;
         }
 
@@ -422,14 +687,12 @@ int main() {
             CloseWindow();
         }
 
-        if(estadoTela == 4){
+        if(estadoTela == 4){            
             BeginDrawing();
 
-            DrawRectangle(75, 275, 650, 250, BLACK);
+            DrawRectangle(75, 275, 650, 340, BLACK);
 
-            DrawText("VOCê VENCEU", 235, 330, 50, GREEN);
-
-            DrawText("Pressione ENTER para voltar ao menu", 110, 430, 30, WHITE);
+            DrawText("VOCê VENCEU", 230, 330, 60, GREEN);
 
             if(IsKeyPressed(KEY_ENTER)){
                 faseAtual = 1;
@@ -440,7 +703,8 @@ int main() {
 
                 estadoTela = 0;
             }
-            
+
+            salvarRanking(rankingSalvo, jogoAtual, ranking, jogoInicio, inserindoNome, nomeDigitado);
             EndDrawing();
             continue;
         }
@@ -448,11 +712,9 @@ int main() {
         if(estadoTela == 5){
             BeginDrawing();
 
-            DrawRectangle(75, 275, 650, 250, BLACK);
+            DrawRectangle(75, 275, 650, 340, BLACK);
 
-            DrawText("GAME OVER", 240, 330, 60, RED);
-
-            DrawText("Pressione ENTER para voltar ao menu", 110, 430, 30, WHITE);
+            DrawText("GAME OVER", 230, 330, 60, RED);
 
             if(IsKeyPressed(KEY_ENTER)){
                 faseAtual = 1;
@@ -464,6 +726,7 @@ int main() {
                 estadoTela = 0;
             }
 
+            salvarRanking(rankingSalvo, jogoAtual, ranking, jogoInicio, inserindoNome, nomeDigitado);
             EndDrawing();
             continue;
         }
@@ -472,7 +735,7 @@ int main() {
             BeginDrawing();
             ClearBackground(BLACK);
 
-            int escolha = MenuDificuldade();
+            int escolha = MenuDificuldade(texture2);
             
             if(escolha == 1){
                 multiplicadorVelocidade = 1.0f;
